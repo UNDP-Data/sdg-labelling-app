0769d40bf6b2dad8f131c46788e5bbc24c94f1ad
Stiene Praet <stiene.praet@undp.org>
2023-05-05 16:35:05 +0200

    Style: change title

--- scripts/components.py
+++ scripts/components.py
@@ -255,7 +255,7 @@ def get_button_container():
 
 def get_body_title():
     return dmc.Text(
-        'SELECT THE MOST RELEVANT SDGs FOR EACH PARAGRAPH',
+        'SELECT ONE OR MORE RELEVANT SDGs FOR EACH PARAGRAPH',
         className='app-body-text',
         color="#1D3557",
         weight=700,
