3e6b8c48e5c7deb57896f7e858ed376befe2cb1f
mykolaskrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2023-05-07 12:49:24 +0200

    Refactor(app): consistently use single quotes

--- app.py
+++ app.py
@@ -8,22 +8,20 @@ from dotenv import load_dotenv; load_dotenv()
 # local packages
 from src import components, callbacks
 
-load_dotenv()
-# Initialize the app
-app = Dash(__name__,
-           external_stylesheets=['styles.css'],
-           meta_tags=[{
-               "name": "viewport",
-               "content": 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5, user-scalable=yes'
+# app definition
+app = Dash(
+    __name__,
+    external_stylesheets=['styles.css'],
+    meta_tags=[{
+       'name': 'viewport',
+       'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5, user-scalable=yes'
 
-           }],
-           prevent_initial_callbacks='initial_duplicate',
-           suppress_callback_exceptions=True
-           )
+    }],
+    prevent_initial_callbacks='initial_duplicate',
+    suppress_callback_exceptions=True,
+)
 
-
-# Initialize components
-
+# define layout
 app.layout = dmc.MantineProvider(
     children=[
         dcc.Store(id='memory-output', storage_type='memory'),
@@ -34,7 +32,6 @@ app.layout = dmc.MantineProvider(
     ]
 )
 
-
-# Run the app
+# run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
