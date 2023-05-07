099d19fabd6c36de227f94cf4075fca373c60cf7
mykolaskrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2023-05-07 14:55:48 +0200

    Feat(styles): add a module containing shared styles

---
+++ src/styles.py
@@ -0,0 +1,27 @@
+def get_sdg_style(sdg_id: int, is_selected: bool) -> dict:
+    style_base = {
+        'background-size': 'cover',
+        'transition': '0.3s',
+        'border': '2px solid transparent',
+        'border-radius': '5px',
+        'cursor': 'pointer',
+    }
+    if is_selected:
+        style_extra = {
+            'height': '11vh',
+            'width': '11vh',
+            'max-height': '11vh',
+            'max-width': '11vh',
+            'background-image': f'url("../assets/SDG_icons/color/en/sdg_{sdg_id}.png")',
+            'box-shadow': 'rgb(38, 57, 77) 0px 20px 30px -10px',
+        }
+    else:
+        style_extra = {
+            'height': '10vh',
+            'width': '10vh',
+            'max-height': '10vh',
+            'max-width': '10vh',
+            'background-image': f'url("../assets/SDG_icons/black/en/sdg_{sdg_id}.png")',
+        }
+    style = style_base | style_extra
+    return style
