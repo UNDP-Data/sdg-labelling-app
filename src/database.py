1044e042fe026c1262c505ded8bbe586f058f5e3
mykolaskrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2023-05-08 10:01:15 +0200

    Refactor(database): rename database fields

--- src/database.py
+++ src/database.py
@@ -20,8 +20,8 @@ def get_paragraph(language, email):
         # Replace null label arrays with empty arrays, so that the size operator is applied correctly
         {
             '$addFields': {
-                'labels': {
-                    '$ifNull': ['$labels', []]
+                'annotations': {
+                    '$ifNull': ['$annotations', []]
                 }
             }
         },
@@ -29,15 +29,15 @@ def get_paragraph(language, email):
         {
             '$match': {
                 'language': language,  # match text langauge
-                'labels': {'$not': {'$elemMatch': {'email': email}}},  # exclude texts labelled by the user
-                'date': {'$lte': datetime.now() - timedelta(minutes=10)},  # exclude texts that have just been labelled
-                '$expr': {'$lte': [{'$size': '$labels'}, 2]}  # exclude texts with already 3 labels
+                'annotations': {'$not': {'$elemMatch': {'email': email}}},  # exclude texts labelled by the user
+                'retrieved_at': {'$lte': datetime.now() - timedelta(minutes=10)},  # exclude texts that have just been labelled
+                '$expr': {'$lte': [{'$size': '$annotations'}, 2]}  # exclude texts with already 3 annotations
             }
         },
         # Add a count field to each document
         {
             '$addFields': {
-                'count': {'$size': '$labels'}
+                'count': {'$size': '$annotations'}
             }
         },
         # Sort by count in descending order
@@ -59,7 +59,7 @@ def get_paragraph(language, email):
 
 def update_queue(_id):
     collection = get_document_collection()
-    collection.update_one({'_id': ObjectId(_id)}, {'$set': {'date': datetime.now()}}, upsert=True)
+    collection.update_one({'_id': ObjectId(_id)}, {'$set': {'retrieved_at': datetime.now()}}, upsert=True)
 
 
 def update_paragraph(_id, labels, email):
@@ -68,21 +68,21 @@ def update_paragraph(_id, labels, email):
     document = collection.find_one({'_id': _id})
 
     if document:
-        if document.get('labels'):
-            aux = list(document['labels'])
+        if document.get('annotations'):
+            aux = list(document['annotations'])
             flag = False
             i = 0
             while i < len(aux) and not flag:
                 if aux[i]['email'] == email:
                     flag = True
-                    aux[i]['user_labels'] = labels
+                    aux[i]['labels'] = labels
                 i += 1
             if not flag:
-                aux.append({'email': email, 'user_labels': labels})
-            document['labels'] = aux
+                aux.append({'email': email, 'labels': labels})
+            document['annotations'] = aux
             collection.replace_one({'_id': _id}, document)
         else:
-            document['labels'] = [{'email': email, 'user_labels': labels}]
+            document['annotations'] = [{'email': email, 'labels': labels}]
             collection.replace_one({'_id': _id}, document)
     else:
         raise Exception('Document not found')
