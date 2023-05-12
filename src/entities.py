b8087718409d5cff7348d6cdd569311b409cd831
mykolaskrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2023-05-13 01:19:41 +0200

    Refactor(entities): separate language type hint

--- src/entities.py
+++ src/entities.py
@@ -2,13 +2,14 @@ from typing import Literal, Optional, Union
 from pydantic import BaseModel, constr
 
 EMAIL = constr(regex=r'.*@undp.org$', to_lower=True)
+LANGUAGE = Literal['ar', 'en', 'fr', 'es', 'ru', 'zh']
 
 
 class Config(BaseModel):
     task_idx: int = 0
     task_ids: list[Union[str, None]]  # track doc ids user has seen in this session
     session_email: EMAIL
-    session_language: Literal['ar', 'en', 'fr', 'es', 'ru', 'zh']
+    session_language: LANGUAGE
 
     def get_task_id(self) -> Union[str, None]:
         task_id = self.task_ids[self.task_idx]
