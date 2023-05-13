3c67ee0d0afd8617d22c41daffc6e0e90695cc07
mykolaskrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2023-05-13 22:41:14 +0200

    Refactor(callbacks): separate out progress bar and button updates into a stand-alone callback

--- src/callbacks.py
+++ src/callbacks.py
@@ -75,17 +75,21 @@ def toggle_modal(n_clicks):
 
 
 @callback(
+    Output('progress-bar', 'value'),
+    Output('progress-bar', 'label'),
     Output('back-button', 'disabled'),
+    Output('next-button', 'children'),
     Input('session-config', 'data'),
 )
-def disable_button_back(config):
-    is_disabled = config['task_idx'] <= 0
-    return is_disabled
+def update_controls(config):
+    config = entities.Config(**config)
+    progress = (sum(task_id is not None for task_id in config.task_ids) - 1) / len(config.task_ids) * 100
+    button_back_disabled = config.task_idx <= 0
+    button_next_name = 'Next & Finish' if config.task_idx == (len(config.task_ids) - 1) is not None else 'Next'
+    return progress, f'{progress:.0f}%', button_back_disabled, button_next_name
 
 
 @callback(
-    Output('progress-bar', 'value'),
-    Output('progress-bar', 'label'),
     Output('chip-container', 'children'),
     Output('paragraph', 'children'),
     Output('content', 'children', allow_duplicate=True),
@@ -96,10 +100,9 @@ def disable_button_back(config):
     State('session-config', 'data'),
     State({'type': 'sdg-button', 'index': ALL}, 'n_clicks'),
     State('comment', 'value'),
-    State('progress-bar', 'value'),
     prevent_initial_call=True
 )
-def update_components(n_clicks_next, n_clicks_back, config, n_clicks_sdgs, comment, progress):
+def update_components(n_clicks_next, n_clicks_back, config, n_clicks_sdgs, comment):
     """Update the components of the main layout and the database with the current state of the labeling."""
     config = entities.Config(**config)
     ctx = callback_context
@@ -113,7 +116,6 @@ def update_components(n_clicks_next, n_clicks_back, config, n_clicks_sdgs, comme
             comment=comment,
         )
         database.update_paragraph(doc_id, annotation)
-        progress = config.task_idx / len(config.task_ids) * 100
     elif ctx.triggered_id == 'back-button':
         config.task_idx = max(config.task_idx - 1, 0)
     else:
@@ -123,7 +125,7 @@ def update_components(n_clicks_next, n_clicks_back, config, n_clicks_sdgs, comme
     # finish the session once the specified number of examples has been labelled
     if config.task_idx == len(config.task_ids):
         final_layout = components.get_finish_layout(reason='session_done')
-        return no_update, no_update, no_update, no_update, final_layout, config.dict(), no_update
+        return no_update, no_update, final_layout, config.dict(), no_update
 
     doc_id = config.get_task_id()
     if doc_id is None:
@@ -133,14 +135,14 @@ def update_components(n_clicks_next, n_clicks_back, config, n_clicks_sdgs, comme
         # finish the session if there are no more unlabelled examples for a given user and language
         if doc is None:
             final_layout = components.get_finish_layout(reason='no_tasks')
-            return no_update, no_update, no_update, no_update, final_layout, config.dict(), no_update
+            return no_update, no_update, final_layout, config.dict(), no_update
     else:
         doc = database.get_paragraph_by_id(doc_id)
         selected_sgds, comment = utils.get_user_label_and_comment(doc, config.session_email)
 
     sdg_buttons = components.get_sdg_buttons(selected_sgds, language=config.session_language)
     config.set_task_id(doc['_id'])
-    return progress, f'{progress:.0f}%', sdg_buttons, doc['text'], no_update, config.dict(), comment
+    return sdg_buttons, doc['text'], no_update, config.dict(), comment
 
 
 @callback(
